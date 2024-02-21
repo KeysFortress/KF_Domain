bff2af02d8aae61d0b3c1660cfcb36af00be4ab8
Kristifor <km@aizen.io>
2024-02-21 17:14:45 +0200

    Adding a model for the challange object

---
+++ lib/models/challange.dart
@@ -0,0 +1,11 @@
+class Challange {
+  String publicKey;
+  String challange;
+  DateTime expiresAt;
+
+  Challange({
+    required this.publicKey,
+    required this.challange,
+    required this.expiresAt,
+  });
+}
