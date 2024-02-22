e83bca35de35531476e9cb0c5c40687434fe7756
Kristifor <km@aizen.io>
2024-02-22 15:09:58 +0200

    Resolving conversion issue between the client and the acting server.

--- lib/converters/binary_converter.dart
+++ lib/converters/binary_converter.dart
@@ -1,17 +1,11 @@
+import 'dart:convert';
+
 class BianaryConverter {
   static String toHex(List<int> data) {
-    return data
-        .map((int value) => value.toRadixString(16).padLeft(2, '0'))
-        .join();
+    return base64.encode(data);
   }
 
   static List<int> hexStringToList(String hexString) {
-    List<int> intList = [];
-    for (int i = 0; i < hexString.length; i += 2) {
-      String hexSubstring = hexString.substring(i, i + 2);
-      int decimalValue = int.parse(hexSubstring, radix: 16);
-      intList.add(decimalValue);
-    }
-    return intList;
+    return base64.decode(hexString);
   }
 }
