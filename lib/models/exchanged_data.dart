ea775314994fcb92087f7a76b6f8955ba0b53e4b
Kristifor Milchev <km@airzen.io>
2024-03-09 12:44:15 +0200

    Adding from json and to json for the exchange data

--- lib/models/exchanged_data.dart
+++ lib/models/exchanged_data.dart
@@ -1,6 +1,23 @@
 class ExchangedData {
   String name;
   int type;
+  bool missing;
 
-  ExchangedData(this.name, this.type);
+  ExchangedData(this.name, this.type, this.missing);
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'type': type,
+      'missing': missing,
+    };
+  }
+
+  factory ExchangedData.fromJson(Map<String, dynamic> json) {
+    return ExchangedData(
+      json['name'] as String,
+      json['type'] as int,
+      json['missing'] as bool,
+    );
+  }
 }
