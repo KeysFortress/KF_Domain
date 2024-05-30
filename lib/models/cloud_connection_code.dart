1506c8948a1059b60ffdd310b0589a5f4fef4b10
Kristifor Milchev <km@airzen.io>
2024-05-30 14:35:48 +0300

    Adding from qr code to the cloud connection model.

--- lib/models/cloud_connection_code.dart
+++ lib/models/cloud_connection_code.dart
@@ -28,6 +28,31 @@ class CloudConnectionCode {
     );
   }
 
+  factory CloudConnectionCode.fromQR(String qrCode) {
+    var parseUri = Uri.parse(qrCode);
+    var url = parseUri.queryParameters["url"];
+    var secret = parseUri.queryParameters["secret"];
+    var setup = parseUri.queryParameters["setup"];
+    var finish = parseUri.queryParameters["finish"];
+    var id = parseUri.queryParameters["id"];
+
+    if (url == null ||
+        setup == null ||
+        finish == null ||
+        secret == null ||
+        id == null) {
+      return CloudConnectionCode.empty();
+    }
+
+    return CloudConnectionCode(
+      url: url,
+      setupUrl: setup,
+      finish: finish,
+      secret: secret,
+      id: id,
+    );
+  }
+
   Map<String, dynamic> toJson() {
     return {
       'url': url,
